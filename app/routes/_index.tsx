f4bf64b285fc9d073c5496f108c58ad37a4f6d41
Robb Traister <rtraister@citrine.io>
2023-08-23 12:55:32 -0400

    use latest remix with redirectDocument builtin and remove patches

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,20 +1,11 @@
 import type { ActionArgs } from "@remix-run/server-runtime";
-import { redirect } from "@remix-run/server-runtime";
+import { redirectDocument } from "@remix-run/server-runtime";
 import { Form } from "@remix-run/react";
-
-const redirectWithReload = (url: string, init?: number | ResponseInit | undefined) => {
-  if (typeof init !== "number") {
-    init ||= {};
-    init.headers = new Headers(init.headers);
-    init.headers.set("X-Remix-Reload-Document", "true");
-  }
-  return redirect(url, init);
-}
 
 export async function action(args: ActionArgs) {
   const data = await args.request.formData();
   return data.get("action") === "redirect"
-    ? redirectWithReload("/external")
+    ? redirectDocument("/external")
     : null;
 }
 
